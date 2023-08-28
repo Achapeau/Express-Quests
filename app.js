f3e9a298e05a9e25613de821ba9d6a16e0845805
Achapeau <127884427+Achapeau@users.noreply.github.com>
2023-08-28 10:35:08 +0200

    express 08

--- app.js
+++ app.js
@@ -15,21 +15,37 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
+const {hashPassword, verifyPassword, verifyToken } = require("./auth")
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
-
-const userHandlers = require("./userHandlers");
-const {hashPassword} = require("./auth")
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 app.post("/api/users", hashPassword, userHandlers.postUser);
+
+app.post("/api/login", userHandlers.getUserByEmailWithPasswordAndPassToNext,
+verifyPassword);
+
+app.use(verifyToken);
+
+app.post("/api/movies", verifyToken, movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+
+
 app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
+
+// const isItDwight = (req, res) => {
+//   if (req.body.email === "dwight@theoffice.com" && req.body.password === "123456") {
+//     res.send('Credentials are valid')
+//   } else {
+//     res.sendStatus(401)
+//   }
+// }
+
 
 app.listen(port, (err) => {
   if (err) {
